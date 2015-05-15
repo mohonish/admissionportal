1162b84b2559bf4d090994c9966d9addd675da72
mohonish <mohonish.c@gmail.com>
2015-05-15 22:00:28 +0530

    untracked file to last commit

---
+++ application/views/admin/applications_view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AOT Admission | Applications</title>
+</head>
+<body>
+   	<h1>Home</h1>
+   	<h2>Welcome <?php echo $username; ?>!</h2>
+   	<h4>Applications</h4>
+   	<a href="home/logout">Logout</a>
+   	<a href="home/changepw">Change Password</a>
+	<!-- Next Feature TBD -->
+	<a href="home/applications">View Applications</a>
+   	
+</body>
+</html>
