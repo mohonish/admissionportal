8f8429d6e8983de58657f868b3772cce55513c4b
mohonish <mohonish.c@gmail.com>
2015-05-16 21:55:53 +0530

    frontend of dashboard done

--- application/views/apply_view.php
+++ application/views/apply_view.php
@@ -1,48 +1,114 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
   <title>AOT Admission | Apply</title>
+  <!-- Bootstrap Core CSS -->
   <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
-  <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.11.3.js"); ?>"></script>
-  <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="<?php echo base_url("assets/css/simple-sidebar.css"); ?>" />
+  <!-- Custom Fonts -->
+  <link href="<?php echo base_url('assets/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
-  <h1>AOT Admission | Apply</h1>
-  <?php echo validation_errors(); ?>
-  <?php echo form_open('apply/verifyapplication'); ?>
-    <label for="email">Email:</label>
-    <input type="text" size="20" id="email" name="email" value="<?php echo set_value('email'); ?>" />
-    <br/>
-    <label for="username">Username:</label>
-    <input type="text" size="20" id="username" name="username" value="<?php echo set_value('username'); ?>" />
-    <br/>
-    <label for="password">Password:</label>
-    <input type="password" size="20" id="password" name="password" />
-    <br/>
-    <label for="password">Re-enter Password:</label>
-    <input type="password" size="20" id="repassword" name="repassword" />
-    <br/>
-    <label for="fname">First Name:</label>
-    <input type="text" size="20" id="fname" name="fname" value="<?php echo set_value('fname'); ?>" />
-    <br/>
-    <label for="lname">Last Name:</label>
-    <input type="text" size="20" id="lname" name="lname" value="<?php echo set_value('lname'); ?>" />
-    <br/>
-    <label for="gname">Guardian's Name:</label>
-    <input type="text" size="20" id="gname" name="gname" value="<?php echo set_value('gname'); ?>" />
-    <br/>
-    <label for="stream">Select Stream:</label>
-    <select name="stream">
-      <option value="it" <?php echo set_select('stream', 'it', TRUE); ?> >IT</option>
-      <option value="cse" <?php echo set_select('stream', 'cse', TRUE); ?> >CSE</option>
-      <option value="ece" <?php echo set_select('stream', 'ece', TRUE); ?> >ECE</option>
-      <option value="ee" <?php echo set_select('stream', 'ee', TRUE); ?> >EE</option>
-      <option value="aei" <?php echo set_select('stream', 'aei', TRUE); ?> >AEIE</option>
-      <option value="me" <?php echo set_select('stream', 'me', TRUE); ?> >ME</option>
-    </select>
-    <br/>
-    <input type="submit" value="Submit Application"/>
-  </form>
-  <p>TBA: Divide form into sections.</p>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="<?php echo base_url('/'); ?>">
+                        AOT Admissions
+                    </a>
+                </li>
+                <li>
+                    <a href="<?php echo base_url('/'); ?>">Home</a>
+                </li>
+                <li>
+                    <a href="<?php echo base_url('/#programmes'); ?>">Programmes</a>
+                </li>
+                <li>
+                    <a href="<?php echo base_url('/#placements'); ?>">Placements</a>
+                </li>
+                <li>
+                    <a href="<?php echo base_url('/#campus-life'); ?>">Campus Life</a>
+                </li>
+                <li>
+                    <a href="<?php echo base_url('/#faculty'); ?>">Faculty</a>
+                </li>
+                <li>
+                    <a href="<?php echo base_url('/#contact'); ?>">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span></a>
+                        <h1>Apply</h1>
+                        <div class="input-group form-container">
+                        <?php echo validation_errors(); ?>
+                        <?php echo form_open('apply/verifyapplication'); ?>
+                        <input type="text" class="form-control" placeholder="Email" id="email" name="email" value="<?php echo set_value('email'); ?>" data-toggle="tooltip" data-placement="right" title="Enter a valid Email ID. This will be used for all further communication." />
+                        <br/><br/>
+                        <input type="text" class="form-control" placeholder="Username" id="username" name="username" value="<?php echo set_value('username'); ?>" data-toggle="tooltip" data-placement="right" title="Must be within 4 to 12 characters" />
+                        <br/><br/>
+                        <input type="password" class="form-control" placeholder="Password" id="password" name="password" />
+                        <br/><br/>
+                        <input type="password" class="form-control" placeholder="Re-enter Password" id="repassword" name="repassword" />
+                        <br/><br/>
+                        <input type="text" class="form-control" placeholder="Student's First Name" id="fname" name="fname" value="<?php echo set_value('fname'); ?>" />
+                        <br/><br/>
+                        <input type="text" class="form-control" placeholder="Student's Last Name" id="lname" name="lname" value="<?php echo set_value('lname'); ?>" />
+                        <br/><br/>
+                        <input type="text" class="form-control" placeholder="Guardian's Name" id="gname" name="gname" value="<?php echo set_value('gname'); ?>" />
+                        <br/><br/>
+                        <select name="stream" class="form-control">
+                          <option value="it" <?php echo set_select('stream', 'it', TRUE); ?> >IT</option>
+                          <option value="cse" <?php echo set_select('stream', 'cse', TRUE); ?> >CSE</option>
+                          <option value="ece" <?php echo set_select('stream', 'ece', TRUE); ?> >ECE</option>
+                          <option value="ee" <?php echo set_select('stream', 'ee', TRUE); ?> >EE</option>
+                          <option value="aei" <?php echo set_select('stream', 'aei', TRUE); ?> >AEIE</option>
+                          <option value="me" <?php echo set_select('stream', 'me', TRUE); ?> >ME</option>
+                        </select>
+                        <br/><br/>
+                        <button type="submit" class="btn btn-default">Submit Application</button>
+                        </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.11.3.js"); ?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    $(document).ready(function(){
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+    </script>
+
 </body>
+
 </html>
--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,18 +1,107 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<title>AOT Admission | Dashboard</title>
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
-  	<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.11.3.js"); ?>"></script>
-  	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+  <link rel="stylesheet" href="<?php echo base_url("assets/css/sb-admin.css"); ?>" />
 </head>
+<!--
 <body>
    	<h1>Home</h1>
    	<h2>Welcome <?php echo $username; ?>!</h2>
    	<a href="home/logout">Logout</a>
    	<a href="home/changepw">Change Password</a>
-	<!-- Next Feature TBD -->
    	<a href="home/status">Application Status</a>
    	<a href="home/edit">Edit Profile</a>
 </body>
+-->
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="<?php echo base_url("/"); ?>">AOT Admissions</a>
+            </div>
+            <!-- Top Menu Items -->
+            <ul class="nav navbar-right top-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>Hi, <?php echo $username; ?><b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="home/edit"><i class="fa fa-fw fa-user"></i> Profile</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="home/logout"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li>
+                        <a href="<?php echo base_url("/index.php/home"); ?>"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="home/edit"><i class="fa fa-fw fa-bar-chart-o"></i> Edit Profile</a>
+                    </li>
+                    <li>
+                        <a href="home/status"><i class="fa fa-fw fa-table"></i> Application Status</a>
+                    </li>
+                    <li>
+                        <a href="home/status"><i class="fa fa-fw fa-table"></i> Tuition Fees</a>
+                    </li>
+                    <li>
+                        <a href="home/status"><i class="fa fa-fw fa-table"></i> Help</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url("/#contact"); ?>"><i class="fa fa-fw fa-table"></i> Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Dashboard
+                            <small>Admissions</small>
+                        </h1>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.11.3.js"); ?>"></script>
+    
+    <!-- Bootstrap Core JavaScript -->
+    <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+
+</body>
+
 </html>
